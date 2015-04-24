5057388fbfedc176f22a714557e86ec6b021de5d
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2015-04-23 22:17:23 -0700

    rehaul how shevy works, move setting of variables inside mixins

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+  // Requires
+  require('load-grunt-tasks')(grunt);
+
+  // Empty init allows us to merge by feature, not by function
+  grunt.config.init({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  // Style config
+  grunt.config.merge({
+    autoprefixer: {
+      dist: {
+        files: {
+          'demo/dist/style.css': 'demo/css/style.css'
+        }
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'demo/css/style.css': 'demo/scss/style.scss'
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass', 'autoprefixer'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+
+  // Register Tasks
+  grunt.registerTask('default', ['sass', 'autoprefixer', 'watch']);
+};
