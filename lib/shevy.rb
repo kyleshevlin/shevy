269492e9d1d4fb5a0eddeaf76177ea337f8dd487
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2016-04-06 21:17:23 -0700

    fix typo in lib/shevy.rb; update gitignore

--- lib/shevy.rb
+++ lib/shevy.rb
@@ -5,4 +5,4 @@ shevy_path = File.expand_path('../../core', __FILE__)
 ENV["SASS_PATH"] = [
   ENV["SASS_PATH"],
   shevy_path
-].compact.join(FILE::PATH_SEPARATOR)
+].compact.join(File::PATH_SEPARATOR)
